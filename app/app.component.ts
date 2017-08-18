3b6df84973129189d6c1902c46fc43a470cef696
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-18 09:36:50 -0700

    add functionality to list the animals

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,23 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Local Zoo</h1>
-  </div>  
+
+    <ul>
+      <li *ngFor="let currentAnimal of animals">Species: {{currentAnimal.species}} <br> Name: {{currentAnimal.name}} <br> Age: {{currentAnimal.age}} <br> Diet: {{currentAnimal.diet}} <br> Location: {{currentAnimal.location}} <br> Caretakers: {{currentAnimal.caretakers}} <br> Sex: {{currentAnimal.sex}} <br> Likes: {{currentAnimal.like}} <br> Dislikes: {{currentAnimal.dislike}} <br><br></li>
+    </ul>
+
+  </div>
   `
 })
 
 export class AppComponent {
+  animals: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
+  ];
+}
 
+export class Animal {
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public like: string, public dislike: string) { }
 }
