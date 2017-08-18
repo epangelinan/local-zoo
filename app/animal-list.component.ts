729a7160e7ee62f3c0a18b5e2d596bf8f958a322
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-18 13:27:40 -0700

    add code to filter list of animals using pipes

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,8 +4,14 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allAnimals">All Animals</option>
+    <option value="youngAnimals">Young Animals</option>
+    <option value="matureAnimals" selected="selected">Mature Animals</option>
+  </select>
+
   <ul>
-    <li *ngFor="let currentAnimal of childAnimalList">Species: {{currentAnimal.species}} <br> Name: {{currentAnimal.name}} <br> Age: {{currentAnimal.age}} <br> Diet: {{currentAnimal.diet}} <br> Location: {{currentAnimal.location}} <br> Caretakers: {{currentAnimal.caretakers}} <br> Sex: {{currentAnimal.sex}} <br> Likes: {{currentAnimal.like}} <br> Dislikes: {{currentAnimal.dislike}} <br> <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button><br><br></li>
+    <li *ngFor="let currentAnimal of childAnimalList | completeness:filterByCompleteness">Species: {{currentAnimal.species}} <br> Name: {{currentAnimal.name}} <br> Age: {{currentAnimal.age}} <br> Diet: {{currentAnimal.diet}} <br> Location: {{currentAnimal.location}} <br> Caretakers: {{currentAnimal.caretakers}} <br> Sex: {{currentAnimal.sex}} <br> Likes: {{currentAnimal.like}} <br> Dislikes: {{currentAnimal.dislike}} <br> <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button><br><br></li>
   </ul>
   `
 })
@@ -14,7 +20,13 @@ export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
 
+  filterByCompleteness: string = " ";
+
   editButtonHasBeenClicked(animalToEdit: Animal) {
     this.clickSender.emit(animalToEdit);
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
   }
 }
